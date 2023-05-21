6e9d158a9eb2f4c856d5e5fef037f3698d6f051a
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-21 17:46:34 +0700

    add context_processors

--- _project/views.py
+++ _project/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 
-from apps.blogs.models import Blog, Category
+from apps.blogs.models import Blog
 
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by(
         "updated_at"
     )
     posts = Blog.objects.filter(is_featured=False, status="Published")
     context = {
-        "categories": categories,
         "featured_posts": featured_posts,
         "posts": posts,
     }
