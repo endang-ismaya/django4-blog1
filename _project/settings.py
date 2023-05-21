1efb634223226013311f7a0b2088f3a542172d8a
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-21 08:45:15 +0700

    add media

--- _project/settings.py
+++ _project/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "_project", "static")]
 
 # MEDIA
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "_project/static/media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -5,3 +5,9 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.category_name
