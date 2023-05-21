3cc80ed2c0b69b25a777a6f53da58a3608039285
Endang Ismaya <endang.ismaya@gmail.com>
2023-05-21 09:33:19 +0700

    add blog model

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from apps.blogs.models import Category
+from apps.blogs.models import Blog, Category
+
+
+class BlogAmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "title",
+        "category",
+        "author",
+        "status",
+        "is_featured",
+        "updated_at",
+    )
+    search_fields = ("id", "title", "category__category_name", "status")
+    list_editable = ("is_featured",)
+
 
 admin.site.register(Category)
+admin.site.register(Blog, BlogAmin)
